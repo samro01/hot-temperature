0264772d8e27a16b9a8e0f73287fb5a296da6b51
samro01 <srozario22@gmail.com>
2022-09-04 11:00:14 +0200

    city added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
 const API_KEY = `9acfe2825e7b00426652fb1b15a8e621`;
 
 const loadTemperature = city => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemperature(data));
 }
+
 const displayTemperature = data => {
-    const temperature = document.getElementById('temperature');
-    console.log(data.main.temp);
-    temperature.innerText = data.main.temp;
+    // const temperature = document.getElementById('temperature');
+    // temperature.innerText = data.main.temp;
+    setInnerTextById('temperature',data.main.temp);
+    setInnerTextById('condition', data.weather[0].main);
+    console.log(data.weather[0].main);
 }
+const setInnerTextById = (id, text) =>{
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+} 
+document.getElementById('btn-search').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    // set city
+    document.getElementById('city').innerText = city;
+    loadTemperature(city);
+})
+
 
 loadTemperature('dhaka');
